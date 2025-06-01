6d2de92f877a89973afa234ebb8185081b923ab2
Barasa Ouma <oumabaros@gmail.com>
2025-06-02 02:44:14 +0300

    Added Workflows

--- workflows.sh
+++ workflows.sh
@@ -4,7 +4,7 @@ WORKFLOW_TEMPLATE=$(cat .github/workflow-template.yml)
 # iterate each route in current directory
 # shellcheck disable=SC2045
 for ROUTE in $(ls microservices); do
-    echo "generating workflow for ${ROUTE}"
+    echo "generating workflow for microservices/${ROUTE}"
 
     # replace template route placeholder with route name
     WORKFLOW=$(echo "${WORKFLOW_TEMPLATE}" | sed "s/{{ROUTE}}/${ROUTE}/g")
