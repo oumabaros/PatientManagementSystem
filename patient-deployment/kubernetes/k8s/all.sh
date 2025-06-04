2416ac97adfd7482c4844a5097c98ab6bd6aa93f
Barasa Ouma <oumabaros@gmail.com>
2025-06-04 03:03:40 +0300

    Added kubernetes

--- patient-deployment/kubernetes/k8s/all.sh
+++ patient-deployment/kubernetes/k8s/all.sh
@@ -1,8 +1,8 @@
 kubectl apply -f backing-services/postgresql
 kubectl apply -f backing-services/kafka
 kubectl apply -f services/config-service.yml
-
-
+kubectl apply -f services/api-gateway.yml
+kubectl apply -f services/auth-service.yml
 
 #kubectl delete -f backing-services/kafka
 #kubectl delete -f backing-services/postgresql
